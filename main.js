0659c5e53ec55a84b891258176f024c72f4942ec
miss-gif <ddc.akari@gmail.com>
2024-05-05 01:20:07 +0900

    Add : 추가 설명

---
+++ main.js
@@ -0,0 +1,11 @@
+// 변수 선언 및 값 할당
+var dynamicVar = 10;
+
+// 동적으로 타입이 결정됨
+console.log(typeof dynamicVar); // 출력: number
+
+// 문자열을 할당하여 타입이 자동으로 변함
+dynamicVar = "Hello, world!";
+
+// 다시 동적으로 타입이 결정됨
+console.log(typeof dynamicVar); // 출력: string
